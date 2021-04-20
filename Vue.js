f1055e06dc74f0a6a4c752e86d63038d6c998e55
Atsushi <atsushithesushi@gmail.com>
2021-04-20 12:32:03 -0300

    Create Vue.js

---
+++ Vue.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        geradorn1: Math.floor(Math.random() * 99) + 1,
+        geradorn2: Math.floor(Math.random() * 99) + 1,
+        n3: 0
+    }
+})
